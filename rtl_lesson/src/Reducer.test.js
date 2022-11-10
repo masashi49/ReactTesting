c052453e420ff1de90a21fb3515bdadf5227b38f
yamadamasashi <shisama94@gmail.com>
2022-11-10 09:26:21 +0900

    mode0の時1を足すテストで2がたされていた。incrementniので修正。

--- rtl_lesson/src/Reducer.test.js
+++ rtl_lesson/src/Reducer.test.js
@@ -10,6 +10,7 @@ describe( "Reducer of ReduxtoolKit", () => {
         }
 
         it( "Should increment by 1 with mode 0", () => {
+            initialState.mode = 2
             const action = { type: increment.type } // increment用のactionを呼ぶ
             const state = reducer( initialState, action ) // テスト用のstateを渡し、新しいstateを計算して返す。incrementのmode:0は、1増やす。
             expect( state.value ).toEqual( 2 )
