57accbe1de528d925bc892ee0bb643c85cf8a404
yamadamasashi <shisama94@gmail.com>
2022-11-15 09:50:37 +0900

    fetchJSONの準備

--- rtl_lesson/src/ReduxAsync.js
+++ rtl_lesson/src/ReduxAsync.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 //  state.customCounter.valueと、関数をimport
-import { selectCount, fetchDummy } from './features/customCounter/customCounterSlice'
+import { selectCount, fetchDummy, selectUsername, fetchJSON } from './features/customCounter/customCounterSlice'
 const ReduxAsync = () => {
-    const count = useSelector( selectCount )
+    const count = useSelector( selectCount ) // storeからstateを取ってくる
+    const username = useSelector( selectUsername ) // storeからstateを取ってくる
     const dispatch = useDispatch()
     return (
         <div>
-            <span data-testid="count-value">{ count }</span> {/* data-testidは全部小文字*/}
+            <span data-testid="count-value">{ count }</span> {/* data-testidは全部小文字*/ }
             <button onClick={ () => dispatch( fetchDummy( 5 ) ) }>FetchDummy</button>
+            { username && <h1>{ username }</h1> }
+            <button onClick={ () => dispatch( fetchJSON() ) }>fetchJSON</button> {/* fetchJSONをdispatch経由で実行*/ }
         </div>
     )
 }
